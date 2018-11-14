5cdee79ededb4a8c0343cc2cff38f8f4cf305225
Haley <haley@justek.us>
2018-11-14 15:46:16 +0800

    Code

---
+++ Component/Component/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Component
+//
+//  Created by yuqing huang on 14/11/2018.
+//  Copyright © 2018 Justek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Component/Component/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Component
+//
+//  Created by yuqing huang on 14/11/2018.
+//  Copyright © 2018 Justek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
